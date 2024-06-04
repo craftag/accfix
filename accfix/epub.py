01b8c171d25c762c5c338f93e32679c95d53abf7
Titusz Pan <titusz.pan@gmail.com>
2024-06-04 15:33:33 +0200

    Refactored the Epub class to use double quotes consistently for string literals.

--- accfix/epub.py
+++ accfix/epub.py
@@ -1,4 +1,5 @@
 """Module for handling EPUB files"""
+
 from pathlib import Path
 import shutil
 import tempfile
@@ -7,9 +8,8 @@ import fsspec
 
 
 class Epub:
-
     def __init__(self, path, clone=True):
-        # type: (str, Path) -> Epub
+        # type: (str|Path, bool) -> Epub
         """Opens a EPUB file for reading and writing.
 
         Wraps the epub file in a fsspec zip filesystem for easy access.
@@ -23,7 +23,7 @@ class Epub:
             temp_dir = tempfile.mkdtemp()
             self._clone = Path(temp_dir) / self._path.name
             shutil.copy2(self._path, self._clone)
-        self._fs = fsspec.filesystem('zip', fo=str(self._clone if self._clone else self._path))
+        self._fs = fsspec.filesystem("zip", fo=str(self._clone if self._clone else self._path))
 
     def read(self, epub_relative_path):
         """Read the content of a file from the EPUB.
@@ -31,6 +31,10 @@ class Epub:
         :param epub_relative_path: The relative path of the file within the EPUB.
         :return: The content of the file.
         """
-        with self._fs.open(epub_relative_path, 'r') as file:
+        with self._fs.open(epub_relative_path, "r") as file:
             return file.read()
 
+
+if __name__ == '__main__':
+    epb = Epub("../scratch/test1.epub")
+    print(epb)
