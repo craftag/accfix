83f47713b03275ee6253d0a4c296c3e7896b10f2
Titusz Pan <titusz.pan@gmail.com>
2024-06-04 16:02:06 +0200

    Refactored Epub class to include logging and support reading files with different modes.

--- accfix/epub.py
+++ accfix/epub.py
@@ -1,16 +1,14 @@
 """Module for handling EPUB files"""
-
+from loguru import logger as log
 from pathlib import Path
-from typing import Union
 import shutil
 import tempfile
-
 import fsspec
 
 
 class Epub:
     def __init__(self, path, clone=True):
-        # type: (Union[str, Path], bool) -> Epub
+        # type: (str|Path, bool) -> Epub
         """Opens a EPUB file for reading and writing.
 
         Wraps the epub file in a fsspec zip filesystem for easy access.
@@ -19,26 +17,40 @@ class Epub:
         :param clone: Create a temporary copy of the EPUB file and open that one.
         """
         self._path = Path(path)
+        self.name = self._path.name
+        log.debug(f"Opening EPUB file: {self._path.name}")
         self._clone = None
         if clone:
             temp_dir = tempfile.mkdtemp()
             self._clone = Path(temp_dir) / self._path.name
             shutil.copy2(self._path, self._clone)
+            log.debug(f"Cloning EPUB file to: {self._clone.parent}")
         self._fs = fsspec.filesystem("zip", fo=str(self._clone if self._clone else self._path))
 
     def __repr__(self):
-        return "Epub('{}')".format(self._path.name)
+        return f'Epub("{self._path.name}")'
 
-    def read(self, epub_relative_path):
+    def read(self, path, mode="rb"):
+        # type: (str|Path, str) -> str | bytes
         """Read the content of a file from the EPUB.
 
-        :param epub_relative_path: The relative path of the file within the EPUB.
+        :param path: The relative path of the file within the EPUB.
+        :param mode: The mode to open the file.
         :return: The content of the file.
         """
-        with self._fs.open(epub_relative_path, "r") as file:
+        path = Path(path)
+        log.debug(f"Reading: {self.name}/{path}")
+        with self._fs.open(path.as_posix(), mode=mode) as file:
             return file.read()
 
 
 if __name__ == '__main__':
     epb = Epub("../scratch/test1.epub")
-    print(epb)
+    print(epb.read("mimetype"))
+    c = epb.read("OEBPS/cover.xhtml")
+    print(type(c))
+    c = epb.read("OEBPS/images/cover.jpg")
+    print(type(c))
+    c = epb.read(Path("OEBPS/images/cover.jpg"))
+    print(type(c))
+
